aaebf9c5d136bb674d36b50cd70f4a4c6c3bdfd4
Benjamin-Hock <147809213+Benjamin-Hock@users.noreply.github.com>
2026-03-31 15:32:14 +0200

    Update AI

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ if check_password():
                         """
                         
                         response = client.models.generate_content(
-                            model='gemini-2.5-flash-preview-09-2025', 
+                            model='gemini-2.5-flash', 
                             contents=prompt
                         )
                         
